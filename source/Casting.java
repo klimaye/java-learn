0fb6c46823a1c3acb1e3a2d09f7034cb78214cd9
Kshitij Limaye <klimaye@gmail.com>
2013-09-18 16:21:16 -0500

    added few more test classes for casting, hex and octal

---
+++ source/Casting.java
@@ -0,0 +1,11 @@
+class Casting {
+    public static void main(String [] args) {
+            long l = 130L;
+            byte b = (byte)l;
+            System.out.println("The byte is " + b);
+            float f = 234.56F;
+            short s = (short)f;
+            System.out.println("The short is " + s);
+    }
+}
+
